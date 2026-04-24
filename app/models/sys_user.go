2e3c9a69f1a665f30462cbbf99816b5cf4151cde
mrezayusufy <mrezayusufy@gmail.com>
2026-04-24 19:03:43 +0330

    system user model created

--- app/models/sys_user.go
+++ app/models/sys_user.go
@@ -1,9 +1,16 @@
 package models
 
 import (
+	"encoding/json"
+
 	"github.com/goravel/framework/database/orm"
 )
 
 type SysUser struct {
 	orm.Model
+	Name            string
+	Email           string
+	EncryptPassword string
+	Role            string
+	Permissions     json.RawMessage
 }
