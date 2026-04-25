d31a56ca334edfc9ba10366bf5a658c3cc41a746
mrezayusufy <mrezayusufy@gmail.com>
2026-04-25 07:47:26 +0330

    created: Category Transaction and Transaction

--- app/models/transaction.go
+++ app/models/transaction.go
@@ -2,4 +2,21 @@ package models
 
 type Transaction struct {
 	BaseModel
+	TenantID        string
+	BranchID        string
+	UserID          string
+	TransactionType string
+	CategoryTrxID   string
+	Amount          float64
+	CurrencyID      string
+	Status          string // pending, completed, failed, reversed, cancalled
+	PaymentMethod   string // cash, card, check, bank_transfer, wallet
+	ReferenceType   string // order, purchase, expence, invoice, etc,
+	ReferenceID     string
+	PartyType       string // customer, vendor, user, bank, cash
+	PartyID         string
+	Note            string
+	ReferenceNumber string
+	IsIcome         bool
+	AccountID       string
 }
